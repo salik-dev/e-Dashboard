a7d3e2425cb891ce44edb9f96886d6c476a7c883
salik-dev <bahadurali0346556@gmail.com>
2023-04-04 01:49:45 +0500

    first commit after refined in api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('./db/config');
+const User = require('./db/userSchema');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is Live!');
+});
+
+app.post('/register', async (req, res) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    console.log(result);
+    res.send(result);
+});
+
+app.listen(4500);
